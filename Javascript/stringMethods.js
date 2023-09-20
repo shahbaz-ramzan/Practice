489c71eec85db13f153dfb175978715793120d65
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-09-20 19:51:06 +0500

    latest code for string methods

--- Javascript/stringMethods.js
+++ Javascript/stringMethods.js
@@ -33,3 +33,49 @@
 // }
 
 // console.log("tolower case:", toLowerCase(string));
+
+// Write a function that takes a string as input and returns the first character of the string.
+// Sample input: "hello"
+// Expected output: "h"
+
+// let string = "hello";
+
+// function getFirst(string) {
+//   return string.charAt(0);
+// }
+// console.log(getFirst(string));
+
+// Write a function that takes a string as input and returns the last character of the string.
+// Sample input: "hello"
+// Expected output: "o"
+
+// let string = "hello";
+
+// function getLast(string) {
+//   let length = string.length;
+//   return string.charAt(length - 1);
+// }
+// console.log(getLast(string));
+
+// 6.Write a function that takes a string as input and returns a new string with the first character of the input string removed.
+// Sample input: "hello"
+// Expected output: "ello"
+
+// let string = "hello";
+
+// function removeString(string) {
+//   return string.slice(1);
+// }
+// console.log(removeString(string));
+
+// 7.Write a function that takes a string as input and returns a new string with the last character of the input string removed.
+// Sample input: "hello"
+// Expected output: "hell"
+
+// let string = "hello";
+
+// function removeLast(string) {
+//   let length = string.length;
+//   return string.slice(0, length - 1);
+// }
+// console.log(removeLast(string));
