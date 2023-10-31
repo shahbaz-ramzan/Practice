4eb4aeb8afd5bf2b7759debf0673fde501af8325
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-10-31 15:59:38 +0500

    reverse array

---
+++ nov-problems-js/problem-01.js
@@ -0,0 +1,13 @@
+// 1. Reverse: Write a function that takes an array and returns a new array with the elements reversed.
+// Example Input: [1, 2, 3]
+// Expected Output: [3, 2, 1]
+
+
+let array=[1,2,3]
+
+function reverse(array){
+	return array.reverse()
+}
+
+const result=reverse(array)
+console.log(result)
