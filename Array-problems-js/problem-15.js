dbd70d20f4ec7dd5c185c07124e56557577d5de5
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-01 19:13:18 +0500

    function that return the array in reverse odder

---
+++ Array-problems-js/problem-15.js
@@ -0,0 +1,14 @@
+// 15.Reverse: Write a function that takes an array and returns a new array with the elements reversed.
+// Example Input: [1, 2, 3]
+// Expected Output: [3, 2, 1]
+
+
+let array=[1,2,3,4,5,6,7]
+
+function reverseOder(array){
+    return array.reverse()
+}
+
+let result=reverseOder(array)
+
+console.log(result)
