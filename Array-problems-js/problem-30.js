d7b44fefad9c2a2340cc20fb6baf7e83c15f1633
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-13 23:21:13 +0500

    remove last element from array

--- Array-problems-js/problem-30.js
+++ Array-problems-js/problem-30.js
@@ -1,3 +1,17 @@
 // 30.Pop: Write a function that takes an array of numbers and removes the last element from the array.
 // Example Input: [1, 2, 3]
 // Expected Output: [1, 2]
+
+
+
+let array=[1,2,3]
+
+
+function removeElemntFromArray(array){
+    array.pop()
+    return array
+}
+
+let result=removeElemntFromArray(array)
+
+console.log(result)
