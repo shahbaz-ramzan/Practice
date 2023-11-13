2f15d3d2646d8afe62e749abe45ba6fa0ebfc54b
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-13 22:50:34 +0500

    function that add element at the end of array

--- Array-problems-js/problem-29.js
+++ Array-problems-js/problem-29.js
@@ -1,3 +1,18 @@
 // 29.Push: Write a function that takes an array of numbers and adds the number 5 to the end of the array.
 // Example Input: [1, 2, 3]
 // Expected Output: [1, 2, 3, 5]
+
+
+
+let array=[1,2,3]
+
+function addElementAtEnd(array){
+     array.push(5)
+     return array
+}
+
+
+let result=addElementAtEnd(array)
+
+
+console.log(result)
