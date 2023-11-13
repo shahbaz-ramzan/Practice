186bf7d8466ade95ac843763cad8006830e76f27
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-13 23:29:46 +0500

    use unshift method and add element at start

--- Array-problems-js/problem-32.js
+++ Array-problems-js/problem-32.js
@@ -1,3 +1,17 @@
 // 32.Unshift: Write a function that takes an array of strings and adds the string 'orange' to the beginning of the array.
 // Example Input: ['apple', 'banana', 'cherry']
 // Expected Output: ['orange', 'apple', 'banana', 'cherry']
+
+
+let array=['apple', 'banana', 'cherry']
+
+
+function addElementAtStart(array){
+    array.unshift("orange")
+
+    return array
+}
+
+let result=addElementAtStart(array)
+
+console.log(result)
