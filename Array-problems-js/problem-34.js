1cf70f7953919908072507c1a5ebb8d3703ce891
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-14 10:30:39 +0500

    flat and array and remove nested array

--- Array-problems-js/problem-34.js
+++ Array-problems-js/problem-34.js
@@ -1,3 +1,15 @@
 // 34.Flat: Write a function that takes a nested array of numbers and returns a new array that is not nested.
 // Example Input: [[1, 2], [3, 4], [5, 6]]
 // Expected Output: [1, 2, 3, 4, 5, 6]
+
+let array=[[1,2],[3,4],[5,6]]
+
+// console.log(array.flat())
+
+function removeNestedArray(array){
+    return array.flat()
+}
+
+let result=removeNestedArray(array)
+
+console.log(result)
