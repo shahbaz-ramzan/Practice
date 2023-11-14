99f7e12c100df74482fb9ba1ddd345ae80f2b199
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-14 12:02:30 +0500

    reverse the array index

--- Array-problems-js/problem-37.js
+++ Array-problems-js/problem-37.js
@@ -1,3 +1,14 @@
 // 37.Reverse: Write a function that takes an array of strings and returns a new array with the strings in reverse order.
 // Example Input: ['apple', 'banana', 'cherry']
 // Expected Output: ['cherry', 'banana', 'apple']
+
+
+let array=["apple", "banana", "cherry"]
+
+function reverse(array){
+    return array.reverse()
+}
+
+let result=reverse(array)
+
+console.log(result)
