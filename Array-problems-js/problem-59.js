136be0870841ca0b66424f1490eae5b9cf55ad52
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-15 13:27:09 +0500

    problem solved 55,57,58,59,60

--- Array-problems-js/problem-59.js
+++ Array-problems-js/problem-59.js
@@ -1,3 +1,18 @@
 // 59. Push: Write a function that takes an array of numbers and adds the number 6 to the end of the array.
 // Example Input: [1, 2, 3, 4, 5]
 // Expected Output: [1, 2, 3, 4, 5, 6]
+
+
+
+let array=[1,2,3,4,5]
+
+function addElement(array){
+     array.push(6)
+     return array
+
+}
+
+
+let result=addElement(array)
+
+console.log(result)
