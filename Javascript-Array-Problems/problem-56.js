20d828ae3ca782194fadaef04ef50ba0af408ebe
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-16 16:48:55 +0500

    problem 49 and 56

--- Javascript-Array-Problems/problem-56.js
+++ Javascript-Array-Problems/problem-56.js
@@ -2,3 +2,17 @@
 // Example Input: [1, 2, 3]
 // Expected Output: true
 
+let array =[1,2,3]
+
+
+
+
+function isArray(array){
+    return Array.isArray(array)
+}
+
+let result=isArray(array)
+
+console.log(result)
+
+
