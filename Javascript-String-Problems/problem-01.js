691f212b83c0738a74e8054ca73172d8eccfd478
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-20 15:27:28 +0500

    function that return the length of string

---
+++ Javascript-String-Problems/problem-01.js
@@ -0,0 +1,15 @@
+// 1.Write a function that takes a string as input and returns the length of the string.
+// Sample input: "Hello World"
+// Expected output: 11
+
+
+let str="Hello World"
+
+
+function findLength(str){
+    return str.length
+}
+
+let result = findLength(str)
+
+console.log(result)
