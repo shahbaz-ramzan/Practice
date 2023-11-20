45271c3c8ac3f8dae8c7de41d05bb5da6b8c2c66
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-20 16:09:37 +0500

    concatenate the two strings

---
+++ Javascript-String-Problems/problem-08.js
@@ -0,0 +1,11 @@
+// 8.Write a function that takes two strings as input and concatenates them.
+// Sample input: "hello", "world"
+// Expected output: "hello world"
+
+function concatenatesSrings(str1,str2){
+    return str1.concat(str2)
+}
+
+let result=concatenatesSrings("hello","world")
+
+console.log(result)
