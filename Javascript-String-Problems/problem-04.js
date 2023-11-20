65e31ec72ee67290243809fc1b2cedc5362f5397
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-20 15:43:55 +0500

    function that return the first char of string

---
+++ Javascript-String-Problems/problem-04.js
@@ -0,0 +1,13 @@
+// 4.Write a function that takes a string as input and returns the first character of the string.
+// Sample input: "hello"
+// Expected output: "h"
+
+
+
+function returnFirstChar(str){
+    return str.charAt(0)
+}
+
+let result=returnFirstChar("hello")
+
+console.log(result)
