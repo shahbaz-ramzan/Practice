8f614e9c2ab1481410e09b66e477516fac02e947
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-11-20 15:35:23 +0500

    convert string to lower case

---
+++ Javascript-String-Problems/problem-03.js
@@ -0,0 +1,14 @@
+// 3.Write a function that takes a string as input and returns the string in lowercase.
+// Sample input: "HELLO WORLD"
+// Expected output: "hello world"
+
+
+let str ="HELLO WORLD"
+
+function makeStrInLowerCase(str){
+    return str.toLowerCase()
+}
+
+let result=makeStrInLowerCase(str)
+
+console.log(result)
